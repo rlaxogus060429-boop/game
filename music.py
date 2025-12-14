ab611622930c948da2120a0ec9c51405a7e46e1c
rlaxogus060429-boop <rlaxogus060429@gmail.com>
2025-12-15 00:12:20 +0900

    Delete music.py

--- music.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-def music_play(music):
-    bgm_path = "./bgm/"
-    pygame.init()
-    pygame.mixer.music.load(f"{bgm_path}{music}.mp3") #Loading File Into Mixer
-    pygame.mixer.music.play(-1) #Playing It In The Whole Device
